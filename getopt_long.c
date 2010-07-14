f1766a5d427ddf612519b4b74ea4981f6e97a803
meddah <meddah@meddah.org>
2010-07-14 22:19:34 +0300

    notes

--- getopt_long.c
+++ getopt_long.c
@@ -8,18 +8,21 @@ int main(int argc, char *argv[])
     int aflag = 0, bflag = 0, cflag = 0, hflag = 0, fflag = 0, oflag = 0, lflag = 0;
     char *barg = NULL;
     char *farg = NULL;
+    char *oarg = NULL;
+    char *larg = NULL;
     int i;
 
     static struct option long_options[] = {
         {"help", no_argument, NULL, 'h'},
         {"file", required_argument, NULL, 'f'},
         {"output", required_argument, NULL, 'o'},
-        {"list", optional_argument, NULL, 'l'},
+        {"list", required_argument, NULL, 'l'},
+
         {0, 0, 0, 0},
     };
 
     opterr = 0; /* global variable */
-    while ((ch = getopt_long(argc, argv, "abh:cf", long_options, NULL)) != -1) {
+    while ((ch = getopt_long(argc, argv, "ab:cf:hl:", long_options, NULL)) != -1) {
         switch (ch) {
             case 'a':
                 aflag = 1;
@@ -31,6 +34,10 @@ int main(int argc, char *argv[])
             case 'o':
                 oflag = 1;
                 oarg = optarg;
+                break;
+            case 'l':
+                lflag = 1;
+                larg = optarg;
                 break;
             case 'f':
                 fflag = 1;
@@ -50,6 +57,9 @@ int main(int argc, char *argv[])
                 else if (optopt == 'f') { /* global variable */
                     fprintf (stderr, "--file seçeneğinin argümanını giriniz\n");
                 }
+                else if (optopt == 'o') { /* global variable */
+                    fprintf (stderr, "--output seçeneğinin argümanını giriniz\n");
+                }
                 else {
                     fprintf(stderr, "-%c: gerçersiz seçenek.", optopt); 
                 }
@@ -62,6 +72,9 @@ int main(int argc, char *argv[])
     printf("%s\n", cflag ? "c seçildi" : "c seçilmedi");
     printf("%s\n", hflag ? "help seçildi" : "help seçilmedi");
     printf("%s (%s)\n", fflag ? "file seçildi" : "file seçilmedi", farg);
+    printf("%s\n", oflag ? "output seçildi" : "output seçilmedi");
+    printf("%s (%s)\n", lflag ? "list seçildi" : "list seçilmedi", larg);
+
 
     for (i = optind; i < argc; ++i) {
         printf("Seçeneksiz argüman: %s\n", argv[i]);
